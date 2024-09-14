2dc3049aabfd797250adc30ffbb6dbd455b4590f
liaakter736 <liaakter736@gmail.com>
2024-09-14 15:40:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slideras').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+ 
+  prevArrow: '<i class="fa-regular fa-circle-left prev"></i>',
+  nextArrow: '<i class="fa-regular fa-circle-right next"></i>',
+
+});
